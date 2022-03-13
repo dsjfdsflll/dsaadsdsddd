cb2fca04452b37fe6438287f376c17919070eac4
dsjfdsflll <98416478+dsjfdsflll@users.noreply.github.com>
2022-03-13 20:00:47 +0300

    dddd

--- 8-Guard 4/models/UserRoles.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserRoles = new mongoose.Schema({
-    guildID: String, 
-    userID: String, 
-    roles: Array
-});
-
-module.exports = mongoose.model('UserRoles', UserRoles);
