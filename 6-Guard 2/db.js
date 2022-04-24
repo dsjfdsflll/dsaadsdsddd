c46dbb51400643dd6a62ce77bde4cba534f39df7
dsjfdsflll <98416478+dsjfdsflll@users.noreply.github.com>
2022-04-24 16:31:32 +0300

    dssdsd

--- 6-Guard 2/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const dark_guard = mongoose.Schema({
-    banlı: Array,
-    owner: Array,
-    bot: Array
-});
-
-module.exports = mongoose.model("Guard", dark_guard);
